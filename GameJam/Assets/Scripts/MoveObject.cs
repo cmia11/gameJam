4277658c7acf0addab768470a117ee96e03f03e2
Claire Abadie <cmia11@hotmail.com>
2020-02-01 00:59:12 +0100

    Script ajouté et texture du curseur

---
+++ GameJam/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float inputX;
+    private float inputY;
+    private float inputZ;
+    private float speed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.up * Time.deltaTime * inputX * speed);
+    }
+}
