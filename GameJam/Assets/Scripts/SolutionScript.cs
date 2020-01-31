f0bcd6c63ad68cb8edbf7235d051e45234da4f85
Your Name <you@example.com>
2020-02-01 00:56:18 +0100

    Objects are prefabs

---
+++ GameJam/Assets/Scripts/SolutionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolutionScript : MonoBehaviour
+{
+    public GameObject repairedObject;
+    public GameObject repairPart;
+    public GameObject solution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
