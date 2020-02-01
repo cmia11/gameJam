e8ddfbd1fdf6f8505a6808bcef0abbe73b5ce028
Claire Abadie <cmia11@hotmail.com>
2020-02-01 01:40:57 +0100

    l'objet dans MoveObject ne peut plus sortir de l'écran

--- GameJam/Assets/Scripts/MoveObject.cs
+++ GameJam/Assets/Scripts/MoveObject.cs
@@ -10,6 +10,12 @@ public class MoveObject : MonoBehaviour
     private float inputY;
     private float inputZ;
     private float speed = 5;
+    private float xMin = -6;
+    private float xMax = 8;
+    private float yMin = 0 ;
+    private float yMax = 7;
+    private float zMin = -5;
+    private float zMax = 8;
 
     void Start()
     {
@@ -21,6 +27,15 @@ public class MoveObject : MonoBehaviour
     {
         inputX = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * Time.deltaTime * inputX * speed);
+        if (transform.position.x > xMax)
+        {
+            transform.position = new Vector3(8, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < xMin)
+        {
+            transform.position = new Vector3(-6, transform.position.y, transform.position.z);
+        }
 
         // gérer le bug quand ça sort de l'écran
 
@@ -28,20 +43,28 @@ public class MoveObject : MonoBehaviour
         transform.Translate(Vector3.right * Time.deltaTime * inputY * speed);
         
         //gérer le bug lorsque ça sort de l'écran
-        //if (transform.position.y < 7)
-        //{
-        //    transform.position = new Vector3(transform.position.x, 7, transform.position.z);
-        //}
+        if (transform.position.y > yMax)
+        {
+            transform.position = new Vector3(transform.position.x, 7, transform.position.z);
+        }
         
         // gérer le bug lorsque ça sort de l'écran
-        //if (transform.position.y > 0)
-        //{
-        //    transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-        //}
+        if (transform.position.y < yMin)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
 
         inputZ = Input.GetAxis("Mouse ScrollWheel");
         transform.Translate(Vector3.forward * Time.deltaTime * inputZ * speed);
 
-        
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 8);
+        }
+
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+        }
     }
 }
