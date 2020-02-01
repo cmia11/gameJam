e19c94859c0c236c8ed3542137f028504bae904e
Your Name <you@example.com>
2020-02-01 02:27:09 +0100

    Implement solution test

--- GameJam/Assets/Scripts/SolutionScript.cs
+++ GameJam/Assets/Scripts/SolutionScript.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class SolutionScript : MonoBehaviour
 {
-    public GameObject repairedObject;
-    public GameObject repairPart;
-    public GameObject solution;
-
+    public GameObject[] solutions;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject repairedObject = GameObject.FindGameObjectWithTag("repairedObject");
+        GameObject repairPart = GameObject.FindGameObjectWithTag("repairPart");
+
+        if (repairedObject == null || repairPart == null)
+        {
+            return;
+        }
+
+        float distance = (repairedObject.transform.position - repairPart.transform.position).sqrMagnitude;
+        float rotationDistance = (repairedObject.transform.rotation.eulerAngles - repairPart.transform.rotation.eulerAngles).sqrMagnitude;
+
+        if (distance < 0.15 && rotationDistance < 0.01)
+        {
+            print("Success");
+            repairedObject.SetActive(false);
+            repairPart.SetActive(false);
+            solutions[0].transform.SetPositionAndRotation(repairedObject.transform.position, repairedObject.transform.rotation);
+            solutions[0].SetActive(true);
+        } else
+        {
+            print(distance + " ----- " + rotationDistance);
+        }
     }
 }
