f6083aec77053f5864278f7a9aee112a7f95fd9d
Your Name <you@example.com>
2020-02-01 14:56:52 +0100

    Add simple GameState

---
+++ GameJam/Assets/Scripts/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public int score;
+    public float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+    }
+}
