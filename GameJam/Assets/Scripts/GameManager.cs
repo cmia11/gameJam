b3866afd5289d95f26128d43cb64ecf8334bd1af
Claire Abadie <cmia11@hotmail.com>
2020-02-02 14:50:36 +0100

    changement temps affichage

--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeStart = 30;
+        timeStart = 28;
     }
 
     // Update is called once per frame
